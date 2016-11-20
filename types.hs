3a899bf0952c5538775c5d2ac6950e114c46caba
Matthew <matthew@mhoare.co.uk>
2016-11-20 22:42:55 +0000

    Add type definition example

---
+++ types.hs
@@ -0,0 +1,8 @@
+-- Demonstrates type hinting of functions in haskell
+
+-- add takes two integers and returns an integer
+-- Adding a type definition to a function allows the compiler to type check easily
+-- If the function returned an unexpected type it would be caught at compile time
+add :: Int -> Int -> Int
+add x y =
+	x + y
